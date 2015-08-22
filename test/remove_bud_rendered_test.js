6d6c165fca8567be29973900539c4d2df5734803
Taka Okunishi <okunishinishi@gmail.com>
2015-08-23 01:04:39 +0900

    Version incremented to 1.0.1

---
+++ test/remove_bud_rendered_test.js
@@ -0,0 +1,20 @@
+/**
+ * Test case for removeBudRendered.
+ * Runs with nodeunit.
+ */
+
+var removeBudRendered = require('../lib/remove_bud_rendered.js');
+
+exports.setUp = function(done) {
+    done();
+};
+
+exports.tearDown = function(done) {
+    done();
+};
+
+exports['Remove bud rendered'] = function(test){
+
+    test.done();
+};
+
