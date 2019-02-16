44f569dee39d59c2f62a562e8d1d8f4f1d8df026
okunishinishi <okunishinishi@gmail.com>
2019-02-16 15:06:55 +0900

    Version incremented to 3.0.1

--- test/bud_remover_test.js
+++ test/bud_remover_test.js
@@ -5,6 +5,7 @@
 'use strict'
 const BudRemover = require('../lib/bud_remover.js')
 const fs = require('fs')
+const { writeFileAsync } = require('asfs')
 const assert = require('assert')
 const mkdirp = require('mkdirp')
 
@@ -14,16 +15,12 @@ before(() => {
   mkdirp.sync(tmpDir)
 })
 
-it('Bud remover', (done) => {
-  let filename = tmpDir + '/work-file.txt';
-  fs.writeFile(filename, 'foo', (err) => {
-    assert.ifError(err)
-    new BudRemover().remove({
-      path: filename
-    }, function (err) {
-      assert.ifError(err)
-      done()
-    })
+it('Bud remover', async () => {
+  let filename = tmpDir + '/work-file.txt'
+  await writeFileAsync(filename, 'foo')
+  const remover = new BudRemover()
+  await remover.remove({
+    path: filename
   })
 })
 
--- test/remove_bud_rendered_test.js
+++ test/remove_bud_rendered_test.js
@@ -4,24 +4,9 @@
  */
 'use strict'
 
-const removeBudRendered = require('../lib/remove_bud_rendered.js')
-const assert = require('assert')
-const co = require('co')
-
 describe('remove-bud-rendered', function () {
   this.timeout(3000)
 
-  before(() => co(function * () {
-
-  }))
-
-  after(() => co(function * () {
-
-  }))
-
-  it('Remove bud rendered', () => co(function * () {
-
-  }))
 })
 
 /* global describe, before, after, it */
